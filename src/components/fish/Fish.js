3b7a651ff0767b1136149597bfa761b6f4637bd6
Csaba Hermann <hcsabi98@gmail.com>
2020-04-29 21:23:38 +0200

    Added dev dependencies

--- src/components/fish/Fish.js
+++ src/components/fish/Fish.js
@@ -1,7 +1,18 @@
 import {Component} from "react";
 
 class Fish extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            fish : {
+                species : '',
+                weight : 1,
+                address : '',
+                phone : '',
+                email : ''
+            }
+        }
+    }
 }
 
 export default Fish;
--- src/components/fisherman/Fisherman.js
+++ src/components/fisherman/Fisherman.js
@@ -1,7 +1,18 @@
 import {Component} from "react";
 
 class Fisherman extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            fisherman : {
+                name : '',
+                certificateNumber : 1,
+                address : '',
+                phone : '',
+                email : ''
+            }
+        }
+    }
 }
 
 export default Fisherman;
