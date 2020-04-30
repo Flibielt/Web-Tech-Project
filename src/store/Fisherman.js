0112a93bc98f4b17af89bc1eae41fbd7048b470a
Csaba Hermann <hcsabi98@gmail.com>
2020-04-30 15:51:29 +0200

    Created new actions

--- src/store/Fisherman.js
+++ src/store/Fisherman.js
@@ -1,6 +1,8 @@
 import EventEmitter from 'events';
 import dispatcher from "../AppDispatcher";
 
+import axios from 'axios';
+
 class Fisherman extends EventEmitter {
 
     _fishermen = [];
@@ -17,3 +19,25 @@ class Fisherman extends EventEmitter {
         this.removeListener('change', callback);
     }
 }
+
+const fisherman = new Fisherman();
+
+dispatcher.register((action) => {
+    if (action.command.commandType === 'INSERT_FISHERMAN') {
+        let newFisherman = action.command.fisherman;
+        newFisherman.id = Math.round(Math.random() * 1000);
+        fisherman._fishermen.push(newFisherman);
+        fisherman.emitChange();
+    } else if (action.command.commandType === 'GET_FISHERMAN') {
+        axios.get("/fishermen")
+            .then((response) => {
+                fisherman._fishermen = response.data;
+                fisherman.emitChange();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+})
+
+export default fisherman;
