25b4f078b49c30fccc0a810aa2ee3c276b26db1c
Csaba Hermann <hcsabi98@gmail.com>
2020-04-30 20:21:59 +0200

    Implemented constructor and change listener

--- src/components/fisherman/catches/CatchList.js
+++ src/components/fisherman/catches/CatchList.js
@@ -1,7 +1,29 @@
 import React, {Component} from 'react';
+import catchStore from "../../../store/CatchStore";
+import CatchAction from "../../../actions/CatchAction";
 
 class CatchList extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            catches : []
+        };
+        this.onChangeOfCatchList = this.onChangeOfCatchList.bind(this);
+    }
 
+    onChangeOfCatchList() {
+        this.setState({
+            catches : catchStore._catch
+        })
+    }
+
+    componentDidMount() {
+        catchStore.addChangeListener(this.onChangeOfCatchList());
+    }
+
+    componentWillUnmount() {
+        catchStore.removeChangeListener(this.onChangeOfCatchList());
+    }
 }
 
 export default CatchList;
