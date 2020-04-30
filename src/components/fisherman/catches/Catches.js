6874e1657dddf2d831cc2f321762374c63a05932
Csaba Hermann <hcsabi98@gmail.com>
2020-04-30 20:11:20 +0200

    Implemented the new catch form

--- src/components/fisherman/catches/Catches.js
+++ src/components/fisherman/catches/Catches.js
@@ -1,4 +1,5 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
+import CatchAction from "../../../actions/CatchAction";
 
 class Catches extends Component {
     constructor(props, context) {
@@ -8,6 +9,51 @@ class Catches extends Component {
             weight : 1,
             species : ''
         }
+    }
+
+    render() {
+        return (
+            <table className="table">
+                <tbody>
+                <tr>
+                    <td>Location</td>
+                    <td><input
+                        type="text"
+                        value={this.state.location}
+                        onChange={(e)=>{
+                            this.setState({location : e.target.value});
+                        }}
+                    /></td>
+                </tr>
+                <tr>
+                    <td>Species</td>
+                    <td><input
+                        type="text"
+                        value={this.state.species}
+                        onChange={(e)=>{
+                            this.setState({species : e.target.value});
+                        }}
+                    /></td>
+                </tr>
+                <tr>
+                    <td>Weight</td>
+                    <td><input
+                        type="number"
+                        value={this.state.weight}
+                        onChange={(e)=>{
+                            this.setState({weight : e.target.value});
+                        }}
+                    /></td>
+                </tr>
+                <tr>
+                    <td><button
+                        className="btn btn-info"
+                        onClick={()=>{CatchAction.newCatch(this.state)}}
+                    >Add</button></td>
+                </tr>
+                </tbody>
+            </table>
+        );
     }
 }
 
