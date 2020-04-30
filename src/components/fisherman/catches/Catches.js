bcd883b504207853bc7801fb338d5e3d1d2fdd41
Csaba Hermann <hcsabi98@gmail.com>
2020-04-30 20:05:10 +0200

    Implemented constructor

--- src/components/fisherman/catches/Catches.js
+++ src/components/fisherman/catches/Catches.js
@@ -1,7 +1,14 @@
 import React, {Component, Fragment} from 'react';
 
 class Catches extends Component {
-
+    constructor(props, context) {
+        super(props, context);
+        this.state={
+            location : '',
+            weight : 1,
+            species : ''
+        }
+    }
 }
 
 export default Catches;
