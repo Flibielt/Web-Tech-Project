f47080146bbf10399e2930def3e91a3e57c31400
Csaba Hermann <hcsabi98@gmail.com>
2020-05-01 12:45:25 +0200

    Test catch list

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.scss';
 
 import Layout from "./components/Layout";
+import CatchForm from "./components/fisherman/catches/CatchForm";
+import CatchList from "./components/fisherman/catches/CatchList";
 
 function App() {
   return (
     <div className="App">
       <Layout>
+          <CatchForm/>
+          <CatchList/>
       </Layout>
     </div>
   );
