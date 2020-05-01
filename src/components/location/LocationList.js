0b8ac155ef96757f4297536fb6d9db795d4792b7
Csaba Hermann <hcsabi98@gmail.com>
2020-05-01 19:43:59 +0200

    Implemented render function

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -6,6 +6,7 @@ import LocationAction from "../../actions/LocationAction";
 class LocationList extends Component {
     constructor(props, context) {
         super(props, context);
+        LocationAction.getLocations();
         this.state = {
             locations : []
         }
@@ -24,6 +25,31 @@ class LocationList extends Component {
 
     componentWillUnmount() {
         locationStore.removeChangeListener(this.onChangeOfLocationList);
+    }
+
+    render() {
+        return (
+            <table className="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Owner</th>
+                    <th>Catches in last month</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    this.state.locations.map((location) => {
+                        return (
+                            <Location key={location.id}
+                                      location ={location}
+                                      />
+                        );
+                    })
+                }
+                </tbody>
+            </table>
+        )
     }
 }
 
