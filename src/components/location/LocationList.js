9ed4cb2d6270f2aeddf4ab2e4edb9c4de801fe7a
Csaba Hermann <hcsabi98@gmail.com>
2020-05-01 19:36:15 +0200

    Created LocationList class

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,1 +1,15 @@
-import React from "react";
+import React, {Component} from 'react';
+import locationStore from "../../store/LocationStore";
+import Location from "./Location"
+import LocationAction from "../../actions/LocationAction";
+
+class LocationList extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            locations : []
+        }
+    }
+}
+
+export default LocationList;
