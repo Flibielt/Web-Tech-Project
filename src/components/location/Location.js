7a76d2fd77a3fe2fb60aa137ea71c52f2caba69d
Csaba Hermann <hcsabi98@gmail.com>
2020-05-01 19:07:00 +0200

    Show one location element

--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+const location = (props) => {
+    return (
+        <tr>
+            <td>{props.location.name}</td>
+            <td>{props.location.owner}</td>
+            <td>{props.location.lastMonthCatches}</td>
+        </tr>
+    )
+}
+
+export default location;
