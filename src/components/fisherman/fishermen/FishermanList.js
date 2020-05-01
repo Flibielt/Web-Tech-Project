ad23d5272cd6e32ebf64327e2013abe951f756ed
Csaba Hermann <hcsabi98@gmail.com>
2020-05-01 20:41:08 +0200

    Implemented change listeners

--- src/components/fisherman/fishermen/FishermanList.js
+++ src/components/fisherman/fishermen/FishermanList.js
@@ -6,8 +6,26 @@ import FishermanAction from "../../../actions/FishermanAction";
 class FishermanList extends Component {
     constructor(props, context) {
         super(props, context);
+        FishermanAction.getFishermen();
         this.state = {
             fishermen : []
         };
+        this.onChangeFishermanList = this.onChangeFishermanList.bind(this);
+    }
+
+    onChangeFishermanList() {
+        this.setState({
+            fishermen : fishermanStore._fishermen
+        });
+    }
+
+    componentDidMount() {
+        fishermanStore.addChangeListener(this.onChangeFishermanList);
+    }
+
+    componentWillUnmount() {
+        fishermanStore.removeChangeListener(this.onChangeFishermanList);
     }
 }
+
+export default FishermanList;
