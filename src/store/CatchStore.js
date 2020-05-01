2c72bb56d7104cb6002d3c94581097f01d500f75
Csaba Hermann <hcsabi98@gmail.com>
2020-05-01 10:54:48 +0200

    Fixed wrong URL

--- src/store/CatchStore.js
+++ src/store/CatchStore.js
@@ -3,6 +3,8 @@ import dispatcher from "../AppDispatcher";
 
 import axios from 'axios';
 
+const DATABASE_BASE_URL = 'http://localhost:3001';
+
 class CatchStore extends EventEmitter {
 
     _catches = [];
@@ -26,10 +28,20 @@ dispatcher.register((action) => {
     if (action.command.commandType === 'INSERT_CATCH') {
         let newCatch = action.command.newCatch;
         newCatch.id = Math.round(Math.random() * 1000);
+        newCatch.fishermanId = 1;
+        newCatch.timestamp = new Date();
+        axios.post(DATABASE_BASE_URL + "/catches", newCatch)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((err) => {
+                console.log("Error")
+                console.log(err);
+            })
         catchStore._catches.push(newCatch);
         catchStore.emitChange();
     } else if (action.command.commandType === 'GET_CATCHES') {
-        axios.get("/fisherman/" + action.command.fishermanId + "/catches")
+        axios.get(DATABASE_BASE_URL + "/fisherman/" + action.command.fishermanId + "catches")
             .then((response) => {
                 catchStore._catches = response.data;
                 catchStore.emitChange();
