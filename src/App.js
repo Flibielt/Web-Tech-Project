1da9d20b50d9c8f0f4dd15478594021c3c155127
Csaba Hermann <hcsabi98@gmail.com>
2020-05-01 21:17:49 +0200

    Created routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import './App.scss';
 
 import Layout from "./components/Layout";
 import FishermanInfo from "./components/fisherman/FishermanInfo";
+import Overview from "./components/overview/Overview";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-          <FishermanInfo/>
+          <Switch>
+              <Route path="/overview" exact component={Overview} />
+              <Route path="/profile" exact component={FishermanInfo} />
+              <Route path="/" exact component={Overview} />
+          </Switch>
       </Layout>
     </div>
   );
