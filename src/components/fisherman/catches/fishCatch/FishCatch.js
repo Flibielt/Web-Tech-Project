6f52e6cda74da5a01a0b3c8c2b8579d46a82231e
Csaba Hermann <hcsabi98@gmail.com>
2020-05-01 22:19:00 +0200

    Changed date format

--- src/components/fisherman/catches/fishCatch/FishCatch.js
+++ src/components/fisherman/catches/fishCatch/FishCatch.js
@@ -3,7 +3,7 @@ import React from "react";
 const fishCatch = (props) => (
     <tr>
         <td>{props.catch.location}</td>
-        <td>{props.catch.timestamp.toString()}</td>
+        <td>{new Date(props.catch.timestamp).toUTCString()}</td>
         <td>{props.catch.species}</td>
         <td>{props.catch.weight}</td>
     </tr>
