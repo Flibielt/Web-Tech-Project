8367853e3fe90c78106972f431f08b836517413e
Csaba Hermann <hcsabi98@gmail.com>
2020-05-02 14:11:02 +0200

    Bug fix
    Event handlers

--- src/components/fisherman/profile/UpdateProfileForm.js
+++ src/components/fisherman/profile/UpdateProfileForm.js
@@ -1,39 +1,33 @@
 import React, {Component} from 'react';
 
 import FishermanAction from "../../../actions/FishermanAction";
-import fishermanStore from "../../../store/FishermanStore";
 
 class UpdateProfileForm extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            fisherman : {}
+            equipment: '',
+            bait: '',
+            techniques: '',
+            fishingPoles: ''
         }
-        this.onChangeOfFisherman = this.onChangeOfFisherman.bind(this);
         this.isUpdateButtonDisabled = this.isUpdateButtonDisabled.bind(this);
-    }
-
-    onChangeOfFisherman() {
-        this.setState({
-            fisherman : fishermanStore._fisherman
-        })
-    }
-
-    componentDidMount() {
-        fishermanStore.addChangeListener(this.onChangeOfFisherman)
-    }
-
-    componentWillUnmount() {
-        fishermanStore.removeChangeListener(this.onChangeOfFisherman)
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     isUpdateButtonDisabled() {
-        return this.state.fisherman.firstName.length < 1
-            || this.state.fisherman.lastName.length < 1
-            || this.state.fisherman.equipment.length < 1
-            || this.state.fisherman.bait.length < 1
-            || this.state.fisherman.techniques.length < 1
-            || this.state.fisherman.fishingPoles.length < 1;
+        return !this.state.equipment
+            || !this.state.bait
+            || !this.state.techniques
+            || !this.state.fishingPoles;
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value    });
     }
 
     render() {
@@ -41,81 +35,39 @@ class UpdateProfileForm extends Component {
             <table className="table">
                 <tbody>
                 <tr>
-                    <td>First name</td>
-                    <td><input
-                        type="text"
-                        value={this.state.fisherman.firstName}
-                        onChange={(e)=>{
-                            this.setState({fisherman : {
-                                    firstName : e.target.value
-                                }
-                            });
-                        }}
-                    /></td>
-                </tr>
-                <tr>
-                    <td>Last name</td>
-                    <td><input
-                        type="text"
-                        value={this.state.fisherman.lastName}
-                        onChange={(e)=>{
-                            this.setState({fisherman : {
-                                    firstName : e.target.value
-                                }
-                            });
-                        }}
-                    /></td>
-                </tr>
-                <tr>
                     <td>Equipment</td>
                     <td><input
                         type="text"
-                        value={this.state.fisherman.equipment}
-                        onChange={(e)=>{
-                            this.setState({fisherman : {
-                                    equipment : e.target.value
-                                }
-                            });
-                        }}
+                        value={this.state.equipment}
+                        name="equipment"
+                        onChange={this.handleInputChange}
                     /></td>
                 </tr>
                 <tr>
                     <td>Baits</td>
                     <td><input
                         type="text"
-                        value={this.state.fisherman.bait}
-                        onChange={(e)=>{
-                            this.setState({fisherman : {
-                                    bait : e.target.value
-                                }
-                            });
-                        }}
+                        value={this.state.bait}
+                        name="bait"
+                        onChange={this.handleInputChange}
                     /></td>
                 </tr>
                 <tr>
                     <td>Techniques</td>
                     <td><input
                         type="text"
-                        value={this.state.fisherman.techniques}
-                        onChange={(e)=>{
-                            this.setState({fisherman : {
-                                    techniques : e.target.value
-                                }
-                            });
-                        }}
+                        value={this.state.techniques}
+                        name="techniques"
+                        onChange={this.handleInputChange}
                     /></td>
                 </tr>
                 <tr>
                     <td>Poles</td>
                     <td><input
                         type="text"
-                        value={this.state.fisherman.fishingPoles}
-                        onChange={(e)=>{
-                            this.setState({fisherman : {
-                                    fishingPoles : e.target.value
-                                }
-                            });
-                        }}
+                        value={this.state.fishingPoles}
+                        name="fishingPoles"
+                        onChange={this.handleInputChange}
                     /></td>
                 </tr>
                 <tr>
@@ -126,9 +78,9 @@ class UpdateProfileForm extends Component {
                             className="btn btn-primary"
                             disabled={this.isUpdateButtonDisabled()}
                             onClick={()=>{
-                                FishermanAction.updateFisherman(this.state.fisherman);
+                                FishermanAction.updateFisherman(this.state);
                             }}
-                        >Add catch</button>
+                        >Update profile</button>
                     </td>
                 </tr>
                 </tbody>
